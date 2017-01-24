adfa0235897d6a4b7702e2e39743ed92faba5c64
holuwaseun <oluwaseun.olusi@gmail.com>
2017-01-24 05:08:29 +0100

    FIrst Commit -- MInc

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const url = require('url')
+
+let app = express()
+
+let http = require("http").Server(app)
+
+app.use(express.static(`${__dirname}/public`))
+app.use('/scripts', express.static(`${__dirname}/node_modules`))
+
+app.get("*", function(request, response){
+    response.sendFile(`${__dirname}/public/app/views/index.html`)
+})
+
+http.listen(2284, function(err){
+    if(err)
+    {
+        console.log(err)
+    }
+    else
+    {
+        console.log("Listening on port 2286")
+    }
+})
