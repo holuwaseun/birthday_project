59ee9e843fc92600c8e8de65145df08d1ad07ec4
holuwaseun <oluwaseun.olusi@gmail.com>
2017-01-25 14:11:12 +0100

    Sixth Commit -- MInc

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 angular.module("BirthdayWishes", ["App-Routes", "Controller", "angularMoment", "angular-loading-bar", "countTo"])
 
 .run(['$rootScope', '$state', function($rootScope, $state){
-    $rootScope.birth_date = new Date("01/31/2017 00:00:00")
+    $rootScope.fullname = "Ajayi Oluwabimpe"
+    $rootScope.birth_date = new Date("01/25/2017 10:21:00")
     $rootScope.tickInterval = 1000
 }])
--- public/app/controller/controller.js
+++ public/app/controller/controller.js
@@ -3,45 +3,66 @@ angular.module("Controller", [])
 .controller("IndexController", ['$rootScope', '$scope', '$timeout', '$filter', '$state', 'moment', function($rootScope, $scope, $timeout, $filter, $state, $moment){
     // Declare a new Date Object
     $scope.current_date = new Date()
-    $scope.current_month = ($scope.current_date.getMonth() === $rootScope.birth_date.getMonth())
-    
-    $scope.start_time = new Date(`${$rootScope.birth_date.getMonth() + 1}/01/${$rootScope.birth_date.getFullYear()} 00:00:00`).getTime()
-
-    $rootScope.title = `Birthday Wishes - ${$state.current.title}`
+    $scope.is_birthday = false
 
     // Run a check to see if the expected birth day and month is true
     if(parseInt($scope.current_date.getDate()) === parseInt($rootScope.birth_date.getDate()) && parseInt($scope.current_date.getMonth()) === parseInt($rootScope.birth_date.getMonth()))
     {
-        // Redirect to the greetings page
-        $state.go("main.greeting", null, {reload: true})
-        $rootScope.is_birthday = true
+        if(parseInt($scope.current_date.getHours()) >= parseInt($rootScope.birth_date.getHours()) && parseInt($scope.current_date.getMinutes()) >= parseInt($rootScope.birth_date.getMinutes()))
+        {
+            // Redirect to the greetings page
+            $scope.is_birthday = true
+            $rootScope.title = `${$state.current.title} - Happy Birthday`
+        }
     }
-    else
-    {
-        // Create a countdown clock to display on the home page
-        let duration = 0
-        let tick = () => {
-            $scope.current_date = new Date()
+
+    $scope.current_month = ($scope.current_date.getMonth() === $rootScope.birth_date.getMonth())
+    
+    $scope.start_time = new Date(`${$rootScope.birth_date.getMonth() + 1}/01/${$rootScope.birth_date.getFullYear()} 00:00:00`).getTime()
+
+    $rootScope.title = `${$state.current.title} - Loading...`
+
+    // Create a countdown clock to display on the home page
+    let duration = 0
+    let tick = () => {
+        $scope.current_date = new Date()
             
-            let percent = (($scope.current_date.getTime() - $scope.start_time) / ($rootScope.birth_date.getTime() - $scope.start_time)) * 100
-            $scope.percentage = $filter('number')(percent, 0)
+        let percent = (($scope.current_date.getTime() - $scope.start_time) / ($rootScope.birth_date.getTime() - $scope.start_time)) * 100
+        $scope.percentage = $filter('number')(percent, 2)
             
-            duration = ($rootScope.birth_date.getTime() - $scope.current_date.getTime())
+        duration = ($rootScope.birth_date.getTime() - $scope.current_date.getTime())
             
-            $scope.duration = {
-                M: moment.duration(duration).get("M") > 9 ? moment.duration(duration).get("M") : `0${moment.duration(duration).get("M")}`,
-                w: moment.duration(duration).get("w") > 9 ? moment.duration(duration).get("w") : `0${moment.duration(duration).get("w")}`,
-                d: moment.duration(duration).get("d") > 9 ? moment.duration(duration).get("d") : `0${moment.duration(duration).get("d")}`,
-                h: moment.duration(duration).get("h") > 9 ? moment.duration(duration).get("h") : `0${moment.duration(duration).get("h")}`,
-                m: moment.duration(duration).get("m") > 9 ? moment.duration(duration).get("m") : `0${moment.duration(duration).get("m")}`,
-                s: moment.duration(duration).get("s") > 9 ? moment.duration(duration).get("s") : `0${moment.duration(duration).get("s")}`
+        $scope.duration = {
+            M: moment.duration(duration).get("M") > 9 ? moment.duration(duration).get("M") : `0${moment.duration(duration).get("M")}`,
+            w: moment.duration(duration).get("w") > 9 ? moment.duration(duration).get("w") : `0${moment.duration(duration).get("w")}`,
+            d: moment.duration(duration).get("d") > 9 ? moment.duration(duration).get("d") : `0${moment.duration(duration).get("d")}`,
+            h: moment.duration(duration).get("h") > 9 ? moment.duration(duration).get("h") : `0${moment.duration(duration).get("h")}`,
+            m: moment.duration(duration).get("m") > 9 ? moment.duration(duration).get("m") : `0${moment.duration(duration).get("m")}`,
+            s: moment.duration(duration).get("s") > 9 ? moment.duration(duration).get("s") : `0${moment.duration(duration).get("s")}`
+        }
+
+        // Run a check to see if the expected birth day and month is true
+        if(parseInt($scope.current_date.getDate()) === parseInt($rootScope.birth_date.getDate()) && parseInt($scope.current_date.getMonth()) === parseInt($rootScope.birth_date.getMonth()))
+        {
+            if(parseInt($scope.current_date.getHours()) === parseInt($rootScope.birth_date.getHours()) && parseInt($scope.current_date.getMinutes()) === parseInt($rootScope.birth_date.getMinutes()))
+            {
+                // Redirect to the greetings page
+                $scope.is_birthday = true
+                $rootScope.title = `${$state.current.title} - Happy Birthday`
             }
-            $timeout(tick, $rootScope.tickInterval)
         }
 
         $timeout(tick, $rootScope.tickInterval)
-
-        $rootScope.is_birthday = false
     }
+
+    if(!$scope.is_birthday)
+    {
+        $timeout(tick, $rootScope.tickInterval)
+    }
+
+    let happy_birthday = [
+        'H_orange.png', 'A_blue.png', 'P_red.png', 'P_green.png', 'Y_pink.png',
+        'B_green.png', 'I_blue.png', 'R_brown.png', 'T_pink.png', 'H_blue.png', 'D_red.png', 'A_broew.png', 'Y_blue.png'
+    ]
 
 }])
