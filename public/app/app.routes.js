1fc411f5ede0bc866215521017a2acdfd8d68256
holuwaseun <oluwaseun.olusi@gmail.com>
2017-01-31 18:13:51 +0100

    route change

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -59,22 +59,7 @@ angular.module("App-Routes", ["ui.router"])
             controllerAs: "message",
             onEnter: ($rootScope, $state) => {
                 // Run a check to see if the expected birth day and month is true
-                if(parseInt($rootScope.current_date.getDate()) <= parseInt($rootScope.birth_date.getDate()) && parseInt($rootScope.current_date.getMonth()) <= parseInt($rootScope.birth_date.getMonth()))
-                {
-                    if(parseInt($rootScope.current_date.getHours()) < parseInt($rootScope.birth_date.getHours()))
-                    {
-                        // Redirect to the greetings page
-                        $state.go('index', null, {reload: true})
-                    }
-                    else
-                    {
-                        if(parseInt($rootScope.current_date.getMinutes()) < parseInt($rootScope.birth_date.getMinutes()))
-                        {
-                            // Redirect to the greetings page
-                            $state.go('index', null, {reload: true})
-                        }
-                    }
-                }
+                
                 $rootScope.title = `Birthday Shouts`
                 $rootScope.page_name = `page-message`
             }
